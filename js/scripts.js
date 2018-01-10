f764a575382d2efcd4048800bc5beb7497d56df0
Daniel Munoz <daniel.munozdev@gmail.com>
2018-01-09 18:12:13 -0800

    Add query selectors

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// User-interface (or front-end) logic;
+let ul = document.querySelector('ul');
+let form = document.querySelector('form');
+let input = document.querySelector('input');
+let h1 = document.querySelector('h1');
+
+console.log(ul, form, input, h1);
