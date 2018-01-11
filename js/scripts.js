7f6a914dc44b500f6c47e11f57af7ab7564340f8
Daniel Munoz <daniel.munozdev@gmail.com>
2018-01-10 17:55:00 -0800

    Add click event listener to ul

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,15 @@ let form = document.querySelector('form');
 let input = document.querySelector('input');
 let h1 = document.querySelector('h1');
 
-console.log(ul, form, input, h1);
+function activateItem(event) {
+  console.log(event.target.nodeName);
+  if (event.target.nodeName === 'LI') {
+    h1.textContent = event.target.textContent;
+    for (let i = 0; i < event.target.parentNode.children.length; i++) {
+      event.target.parentNode.children[i].classList.remove('active');
+    }
+    event.target.classList.add('active');
+  }
+}
+
+ul.addEventListener('click', activateItem);
